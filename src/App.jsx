3f5d9f7f69ab10f8d58c6b58fb423f15057881b6
LisaLReynolds <lisalreynolds2015@gmail.com>
2025-04-02 16:11:40 -0600

    removed overlay in css because it was making NavTabs nonfuntional. Changed instances of Nav to NavTabs in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
-import Nav from './components/NavTabs';
+import NavTabs from './components/NavTabs';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'; // Import your custom CSS file
 // import Contact from './pages/Contact'
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
-    <Header><Nav /></Header>
+    <div className="overlay"></div> {/* Add the overlay here */}
+    <Header><NavTabs /></Header>
       <main className="mx-3">
         <Outlet />
         {/* <Contact /> */}
