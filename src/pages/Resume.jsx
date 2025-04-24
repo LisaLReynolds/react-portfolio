4bdbe62899755859d3edefb07d58566295fd85df
LisaLReynolds <lisalreynolds2015@gmail.com>
2025-04-24 14:07:20 -0600

    added content to Resume.jsx. updated hover settings for icons

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,21 +2,54 @@ export default function Resume() {
   return (
     <div>
       <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      
+      <section>
+        <h2 class="aref-ruqaa-bold">SUMMARY</h2>
+        <p>
+          Professional with over 10 years of customer service experience looking
+          to transition into the position of a full-stack developer. Experience
+          building projects in an intensive boot camp and as a Web Development
+          intern has strengthened passions for front-end design and development,
+          as well as accessibility. Eager to continue learning and building.
+        </p>
+      </section>
+      <section>
+        <h2 class="aref-ruqaa-bold">TECHNICAL SKILLS</h2>
+        <p>
+          Agile development, APIs, Bootstrap, CSS3, Express, GitHub, Handlebars,
+          HTML5, JavaScript ES6+, jQuery, JSON, MaterialUI, MERN Stack, MongoDB,
+          Node, NoSQL, Object-oriented Programming, PostgreSQL, React,
+          Sequelize, SQL, Web Accessibility, WordPress
+        </p>
+      </section>
+      <section>
+        <h2 class="aref-ruqaa-bold">EXPERIENCE</h2>
+        <article>
+          <h3>Web Developer Intern</h3>
+          <p>CodeGeek</p>
+          <p>117 E Mountain Ave, Suite 222, Fort Collins, CO 80524</p>
+          <ul>
+            <li>
+              Performed monthly updates for WordPress websites developed and
+              maintained by CodeGeek. Included performing backups, updating, and
+              both manual and automated testing of sites.
+            </li>
+            <li>
+              Assessed and addressed accessibility issues found by Accessibility
+              Checker Plugin.
+            </li>
+            <li>
+              Assisted lead designer with website reviews for potential clients.
+            </li>
+            <li>
+              Created a usability testing document that served to direct project
+              managers on how to conduct usability tests. It also consolidated
+              resources and access to previous usability testing conducted by
+              CodeGeek.
+            </li>
+          </ul>
+        </article>
+      </section>
     </div>
   );
 }
