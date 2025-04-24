c325bbfcbbd701c3610d1ee70b9651df946e59af
LisaLReynolds <lisalreynolds2015@gmail.com>
2025-04-24 13:06:12 -0600

    added email, github and linkedin icons to footer and modified css. Changed svg to fill white

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,22 @@
+import '../App.css'; // Importing the App.css file
+import emailIcon from '../assets/email-svgrepo.svg';
+import githubIcon from '../assets/github-svgrepo.svg';
+import linkedinIcon from '../assets/linkedin-icon.svg';
+
 export default function Footer() {
     return (
-    <footer>this is the footer</footer>
+    <footer>
+        <section className="icons">
+            <a href="mailto:lisalreynolds2015@gmail.com">
+                <img src={emailIcon} alt="email icon" />
+            </a>
+            <a href="https://github.com/LisaLReynolds">
+                <img src={githubIcon} alt="GitHub icon"/>  
+            </a>
+            <a href="https://www.linkedin.com/in/lisalynnreynolds/">     
+                <img src={linkedinIcon} alt="LinkedIn icon"/>
+            </a>
+      </section>
+    </footer>
     )
 }
