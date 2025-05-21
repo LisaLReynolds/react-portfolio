5423ea1daff2d2a330e847790e89dd551c48c8ac
LisaLReynolds <lisalreynolds2015@gmail.com>
2025-05-21 13:53:42 -0600

    created Footer.module.css and moved code from App.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,13 @@ import '../App.css'; // Importing the App.css file
 import emailIcon from '../assets/email-svgrepo.svg';
 import githubIcon from '../assets/github-svgrepo.svg';
 import linkedinIcon from '../assets/linkedin-icon.svg';
+import styles from './Footer.module.css'; // Importing the Footer.module.css file
+
 
 export default function Footer() {
     return (
-    <footer>
-        <section className="icons">
+    <footer className={styles.footer}>
+        <section className={styles.icons}>
             <a href="mailto:lisalreynolds2015@gmail.com">
                 <img src={emailIcon} alt="email icon" />
             </a>
