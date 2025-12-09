0f6ea4f45ff47728bbf0066a78564ec5ad830ec5
LisaLReynolds <lisalreynolds2015@gmail.com>
2025-12-08 17:55:39 -0700

    added another card to portfolio, another div, and some css

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,20 +1,33 @@
 import searchHistoryImg from '../assets/searchHistory.png';
-import styles from './Portfolio.module.css';  // Importing the Portfolio.module.css file
-import '../App.css';  // Importing the App.css file for global styles
+import styles from './Portfolio.module.css';
 
 export default function Portfolio() {
   return (
-    <div>
+    <div className={styles.portfolio}>
       <h1>Portfolio</h1>
-      <div className={`card`} style={{ width: "18rem" }}>
-        <img src={searchHistoryImg} className={`card-img-top`} alt="..." />
-        <div className={`card-body`}>
-          <h5 className={`card-title`}>Something Saucy</h5>
-          <p className={`card-text`}>
-            This website utilizes information from two APIs to generate recipes based on user search input and a random drink selection. A third API is used to generate a random meal recipe if the user clicks yes on the modal prompt.
-          </p>
-         <a href="https://lisalreynolds.github.io/Something-Saucy/" className={`card-link`}>Website</a>
-         <a href="https://github.com/LisaLReynolds/Something-Saucy" className={`card-link`}>Github</a>
+      <div className={styles.cardsContainer}>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={searchHistoryImg} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Something Saucy</h5>
+            <p className="card-text">
+              This website utilizes information from two APIs to generate recipes based on user search input and a random drink selection. A third API is used to generate a random meal recipe if the user clicks yes on the modal prompt.
+            </p>
+            <a href="https://lisalreynolds.github.io/Something-Saucy/" className="card-link">Website</a>
+            <a href="https://github.com/LisaLReynolds/Something-Saucy" className="card-link">Github</a>
+          </div>
+        </div>
+
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={searchHistoryImg} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Something Saucy</h5>
+            <p className="card-text">
+              This website utilizes information from two APIs to generate recipes based on user search input and a random drink selection. A third API is used to generate a random meal recipe if the user clicks yes on the modal prompt.
+            </p>
+            <a href="https://lisalreynolds.github.io/Something-Saucy/" className="card-link">Website</a>
+            <a href="https://github.com/LisaLReynolds/Something-Saucy" className="card-link">Github</a>
+          </div>
         </div>
       </div>
     </div>
